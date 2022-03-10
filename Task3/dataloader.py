06f7dd50a3df74f2629142e533350842a031c49b
Nirav Shenoy <niravshenoy16@gmail.com>
2022-03-10 17:35:08 +0100

    Implementation of FixMatch
    Initial Data Loaders and code structure for Fixmatch

---
+++ Task3/dataloader.py
@@ -0,0 +1,181 @@
+import numpy as np
+import torch
+import math
+import copy
+
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from PIL import Image
+
+cifar10_mean = [0.4914, 0.4822, 0.4465]
+cifar10_std = [0.2471, 0.2435, 0.2616]
+cifar100_mean = [0.5071, 0.4867, 0.4408]
+cifar100_std = [0.2675, 0.2565, 0.2761]
+
+
+def weak_augmentation(mean, std, train=True):
+    if train:
+        return transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(size=32,
+                                  padding=int(32*0.125),
+                                  padding_mode='reflect'),
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std)
+        ])
+    else:
+        return transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std)
+        ])  
+
+def x_u_split(args, labels):
+    label_per_class = args.num_labeled // args.num_classes
+    labels = np.array(labels)
+    labeled_idx = []
+    unlabeled_idx = np.array(range(len(labels)))
+    for i in range(args.num_classes):
+        idx = np.where(labels == i)[0]
+        idx = np.random.choice(idx, label_per_class, False)
+        labeled_idx.extend(idx)
+    labeled_idx = np.array(labeled_idx)
+    assert len(labeled_idx) == args.num_labeled
+
+    if args.expand_labels or args.num_labeled < args.train_batch:
+        num_expand_x = math.ceil(
+            args.train_batch * args.iter_per_epoch / args.num_labeled)
+        labeled_idx = np.hstack([labeled_idx for _ in range(num_expand_x)])
+    np.random.shuffle(labeled_idx)
+    return labeled_idx, unlabeled_idx
+
+
+def get_cifar10(args, root):
+    transform_labeled = weak_augmentation(cifar10_mean, cifar10_std, True)
+    transform_val = weak_augmentation(cifar10_mean, cifar10_std, False)
+    base_dataset = datasets.CIFAR10(root, train=True, download=True)
+
+    train_labeled_idxs, train_unlabeled_idxs = x_u_split(
+        args, base_dataset.targets)
+
+    train_labeled_dataset = CIFAR10SSL(
+        root, train_labeled_idxs, train=True,
+        transform=transform_labeled)
+
+    train_unlabeled_dataset = CIFAR10SSL(
+        root, train_unlabeled_idxs, train=True,
+        transform=transform_labeled, is_strong_augment=True)
+
+    test_dataset = datasets.CIFAR10(
+        root, train=False, transform=transform_val, download=False)
+
+    return train_labeled_dataset, train_unlabeled_dataset, test_dataset
+
+
+def get_cifar100(args, root):
+
+    transform_labeled = weak_augmentation(cifar100_mean, cifar100_std, True)
+
+    transform_val = weak_augmentation(cifar100_mean, cifar100_std, False)
+
+    base_dataset = datasets.CIFAR100(
+        root, train=True, download=True)
+
+    train_labeled_idxs, train_unlabeled_idxs = x_u_split(
+        args, base_dataset.targets)
+
+    train_labeled_dataset = CIFAR100SSL(
+        root, train_labeled_idxs, train=True,
+        transform=transform_labeled)
+
+    train_unlabeled_dataset = CIFAR100SSL(
+        root, train_unlabeled_idxs, train=True,
+        transform=transform_labeled, is_strong_augment=True)
+
+    test_dataset = datasets.CIFAR100(
+        root, train=False, transform=transform_val, download=False)
+
+    return train_labeled_dataset, train_unlabeled_dataset, test_dataset
+
+
+class CIFAR10SSL(datasets.CIFAR10):
+    def __init__(self, root, indexs, train=True,
+                 transform=None, target_transform=None,
+                 download=False, is_strong_augment=False, strong_augment=None):
+        super().__init__(root, train=train,
+                         transform=transform,
+                         target_transform=target_transform,
+                         download=download)
+        if indexs is not None:
+            self.data = self.data[indexs]
+            self.targets = np.array(self.targets)[indexs]
+            self.transform = transform
+            self.is_strong_augment = is_strong_augment
+            if self.is_strong_augment:
+                if strong_augment is None:
+                    self.strong_augment = transforms.RandAugment(3, 4)
+                else:
+                    self.strong_augment = strong_augment
+
+    def __getitem__(self, index):
+        img, target = self.data[index], self.targets[index]
+        img = Image.fromarray(img)
+        img_strong = img
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        target = torch.tensor(target)
+
+        if self.is_strong_augment:
+            img_strong = self.strong_augment(img)
+            print('Weak = {}, Strong = {}'.format(torch.tensor(img).dtype, torch.tensor(img_strong).dtype))
+            return torch.tensor(img), torch.tensor(img_strong).to(torch.uint8), target.long()
+
+        return torch.tensor(img), target.long()
+
+
+class CIFAR100SSL(datasets.CIFAR100):
+    def __init__(self, root, indexs, train=True,
+                 transform=None, target_transform=None,
+                 download=False, is_strong_augment=False, strong_augment=None):
+        super().__init__(root, train=train,
+                         transform=transform,
+                         target_transform=target_transform,
+                         download=download)
+        if indexs is not None:
+            self.data = self.data[indexs]
+            self.targets = np.array(self.targets)[indexs]
+
+    def __getitem__(self, index):
+        img, target = self.data[index], self.targets[index]
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            img = self.transform(img)
+            ad = AugmentedDataset(img, transform=self.transform,
+                                   is_strong_augment=self.is_strong_augment, strong_augment=self.strong_augment)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        target = torch.tensor(target)
+        return torch.tensor(img), target.long()
+
+
+# class AugmentedDataset(Dataset):
+#     def __init__(self, data, target=None, num_classes=None, transform=None, is_strong_augment=False, strong_augment=None, *args, **kwargs):
+#         super(AugmentedDataset, self).__init__()
+#         self.data = data
+#         self.target = target
+#         self.transform = transform
+#         self.is_strong_augment = is_strong_augment
+#         if self.is_strong_augment:
+#             if self.strong_augment is None:
+#                 self.strong_augment = transforms.RandAugment(3, 4)
+#             else:
+#                 self.strong_augment = strong_augment
+
+#     def __getitem__(self, idx):
---
+++ Task3/main.py
@@ -0,0 +1,336 @@
+#!/usr/bin/env python3
+
+import argparse
+import math
+import os
+import logging
+import matplotlib.pyplot as plt
+import numpy as np
+
+from dataloader import get_cifar10, get_cifar100
+from test import test_cifar10, test_cifar100
+from utils import accuracy
+
+from model.wrn import WideResNet
+
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+curr_path = os.path.dirname(os.path.abspath(__file__))
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] - %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S', filename=os.path.join(curr_path, 'out.task1.log'))
+
+def main(args):
+    if args.dataset == "cifar10":
+        args.num_classes = 10
+        labeled_dataset, unlabeled_dataset, test_dataset = get_cifar10(args,
+                                                                       args.datapath)
+    if args.dataset == "cifar100":
+        args.num_classes = 100
+        labeled_dataset, unlabeled_dataset, test_dataset = get_cifar100(args,
+                                                                        args.datapath)
+    args.epoch = math.ceil(args.total_iter / args.iter_per_epoch)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    labeled_loader = iter(DataLoader(labeled_dataset,
+                                     batch_size=args.train_batch,
+                                     shuffle=True,
+                                     num_workers=args.num_workers))
+    unlabeled_loader = iter(DataLoader(unlabeled_dataset,
+                                       batch_size=args.train_batch,
+                                       shuffle=True,
+                                       num_workers=args.num_workers))
+    test_loader = DataLoader(test_dataset,
+                             batch_size=args.test_batch,
+                             shuffle=False,
+                             num_workers=args.num_workers)
+
+    model = WideResNet(args.model_depth,
+                       args.num_classes, widen_factor=args.model_width)
+    model = model.to(device)
+
+    init_path = os.path.join(curr_path, 'init_model.pt')
+    torch.save(model.state_dict(), init_path)
+
+    def save_checkpoint(checkpoint, best_path):
+        logging.info('Saving model of epoch %s with validation accuracy = %.3f and loss = %.3f',
+                    checkpoint['epoch'], checkpoint['validation_accuracy'], checkpoint['validation_loss'])
+        torch.save(checkpoint, best_path)
+
+    try:
+        # labeled data
+        x_l, y_l = next(labeled_loader)
+        img = x_l.reshape(-1,3,32,32)
+        img = torch.transpose(torch.transpose(img, 1, 2), 2, 3)
+        fig, axes1 = plt.subplots(5,5,figsize=(3,3))
+        i=0
+        for j in range(5):
+            for k in range(5):
+                axes1[j][k].set_axis_off()
+                axes1[j][k].imshow(img[i:i+1][0])
+                plt.show()
+                i += 1
+    except StopIteration:
+        labeled_loader = iter(DataLoader(labeled_dataset,
+                                            batch_size=1,
+                                            shuffle=True,
+                                            num_workers=args.num_workers))
+        x_l, y_l = next(labeled_loader)
+
+    try:
+        # unlabeled data        
+        x_ul_w, x_ul_s, _ = next(unlabeled_loader)
+        img_weak = x_ul_w.reshape(-1,3,32,32)
+        img_strong = x_ul_s.reshape(-1,3,32,32)
+        img_weak = torch.transpose(torch.transpose(img_weak, 1, 2), 2, 3)
+        img_strong = torch.transpose(torch.transpose(img_strong, 1, 2), 2, 3)
+        fig, axes1 = plt.subplots(5,5,figsize=(3,3))
+        fig1, axes2 = plt.subplots(5,5,figsize=(3,3))
+        i=0
+        for j in range(5):
+            for k in range(5):
+                axes1[j][k].set_axis_off()
+                axes1[j][k].imshow(img_weak[i:i+1][0])
+                axes2[j][k].set_axis_off()
+                axes2[j][k].imshow(img_strong[i:i+1][0])
+                plt.show()
+                i += 1
+    except StopIteration:
+        unlabeled_loader = iter(DataLoader(unlabeled_dataset,
+                                            batch_size=1,
+                                            shuffle=True,
+                                            num_workers=args.num_workers))
+
+    criterion = nn.CrossEntropyLoss()
+    exit()
+
+    for threshold in threshold_list:
+        model.load_state_dict(torch.load(init_path))
+        optimizer = optim.SGD(params=model.parameters(), lr=args.lr,
+                              momentum=args.momentum, weight_decay=args.wd)
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [15, 40, 65, 90])
+        best_loss = float('inf')
+        best_path = os.path.join(
+            curr_path, 'best_model' + str(int(threshold*100)) + '.pt')
+        logging.info('Model Parameters for threshold %s',
+                    threshold)
+        loss_list = []
+        for epoch in range(args.epoch):
+            model.train()
+            
+
+'''
+    init_path = os.path.join(curr_path, 'init_model.pt')
+    torch.save(model.state_dict(), init_path)
+
+    def save_checkpoint(checkpoint, best_path):
+        logging.info('Saving model of epoch %s with validation accuracy = %.3f and loss = %.3f',
+                    checkpoint['epoch'], checkpoint['validation_accuracy'], checkpoint['validation_loss'])
+        torch.save(checkpoint, best_path)
+
+    criterion = nn.CrossEntropyLoss()
+    
+    #threshold_list = [0.6, 0.75, 0.95]
+    threshold_list = [0.6]
+
+    for threshold in threshold_list:
+        model.load_state_dict(torch.load(init_path))
+        optimizer = optim.SGD(params=model.parameters(), lr=args.lr,
+                              momentum=args.momentum, weight_decay=args.wd)
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [15, 40, 65, 90])
+        best_loss = float('inf')
+        best_path = os.path.join(
+            curr_path, 'best_model' + str(int(threshold*100)) + '.pt')
+        logging.info('Model Parameters for threshold %s',
+                    threshold)
+        loss_list = []
+        for epoch in range(args.epoch):
+            model.train()
+            x_pseudo_set = []
+            y_pseudo_set = []
+            correct = 0
+            total = 0
+            running_loss = 0.0
+
+            for i in range(args.iter_per_epoch):
+                try:
+                    # labeled data
+                    x_l, y_l = next(labeled_loader)
+                except StopIteration:
+                    labeled_loader = iter(DataLoader(labeled_dataset,
+                                                     batch_size=args.train_batch,
+                                                     shuffle=True,
+                                                     num_workers=args.num_workers))
+                    x_l, y_l = next(labeled_loader)
+
+                try:
+                    # unlabeled data
+                    x_ul, _ = next(unlabeled_loader)
+                except StopIteration:
+                    unlabeled_loader = iter(DataLoader(unlabeled_dataset,
+                                                       batch_size=args.train_batch,
+                                                       shuffle=True,
+                                                       num_workers=args.num_workers))
+                    x_ul, _ = next(unlabeled_loader)
+
+                x_l, y_l = x_l.to(device), y_l.to(device)
+                x_ul = x_ul.to(device)
+
+                # concatenate labeled and unlabeled
+                if x_pseudo_set:
+                    x_pseudo_tensor = torch.stack(x_pseudo_set).to(device)
+                    x_l = torch.cat((x_l, x_pseudo_tensor))
+                    y_l = torch.cat(
+                        (y_l, torch.tensor(y_pseudo_set).to(device)))
+
+                # train model
+                y_pred_l = model(x_l)
+
+                # compute loss
+                correct += (torch.argmax(y_pred_l, axis=1)
+                            == y_l).float().sum()
+                total += float(x_l.size(dim=0))
+
+                loss = criterion(y_pred_l, y_l)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
+
+                # predict unlabeled
+                y_pseudo_pred = model(x_ul)
+
+                # add to subset if probability is greater than threshold
+                y_pseudo_label_prob, y_pseudo_label_class = torch.max(
+                    y_pseudo_pred, axis=1)
+
+                x_pseudo_set = []
+                y_pseudo_set = []
+                for k, row in enumerate(y_pseudo_label_prob):
+                    if row >= threshold:
+                        x_pseudo_set.append(x_ul[k, :, :, :])
+                        y_pseudo_set.append(y_pseudo_label_class[k])
+                # End of batch
+
+            accuracy = 100 * correct / total
+            running_loss /= args.iter_per_epoch
+            loss_list.append(running_loss)
+
+            with torch.no_grad():
+                model.eval()
+                test_loss = 0.0
+                correct = 0.0
+                for j, (x_v, y_v) in enumerate(test_loader):
+                    x_v, y_v = x_v.to(device), y_v.to(device)
+                    y_op_val = model(x_v)
+                    loss = criterion(y_op_val, y_v)
+
+                    test_loss += loss.item()
+                    _, y_pred_test = y_op_val.max(1)
+                    correct += y_pred_test.eq(y_v).sum()
+
+                test_accuracy = 100 * correct.float() / len(test_loader.dataset)
+                test_loss = test_loss / j
+
+                logging.info("Epoch %s/%s, Train Accuracy: %.3f, Test Accuracy: %.3f, Training Loss: %.3f, Test Loss: %.3f",
+                    epoch+1,
+                    args.epoch,
+                    accuracy.item(),
+                    test_accuracy,
+                    running_loss,
+                    test_loss
+                )
+
+                if test_loss < best_loss:
+                    best_loss = test_loss
+                    checkpoint = {
+                        'epoch': epoch+1,
+                        'threshold': threshold,
+                        'validation_loss': test_loss,
+                        'validation_accuracy': test_accuracy,
+                        'state_dict': model.state_dict(),
+                    }
+                    save_checkpoint(checkpoint, best_path)
+            scheduler.step()
+            print("Epoch {}/{}, Train Accuracy: {:.3f}, Test Accuracy: {:.3f}, Training Loss: {:.3f}, Test Loss: {:.3f}".format(
+                    epoch+1,
+                    args.epoch,
+                    accuracy.item(),
+                    test_accuracy,
+                    running_loss,
+                    test_loss
+                ))
+
+        logging.info('Training Complete...')
+
+        # plt.plot(loss_list)
+        # loss_function = os.path.join(
+            # curr_path, 'loss' + str(int(threshold*100)) + '.png')
+        # plt.savefig(loss_function)
+        # plt.close()
+        # Model Evaluation
+        logging.info('Evalutating Model for Threshold = %s', threshold)
+        if args.dataset == "cifar10":
+            test_cifar10(args, device, test_loader, best_path)
+        elif args.dataset == "cifar100":
+            test_cifar100(args, device, test_loader, best_path)
+            '''
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Pseudo labeling \
+                                        of CIFAR10/100 with pytorch")
+
+    # Dataset
+    parser.add_argument("--dataset", default="cifar10",
+                        type=str, choices=["cifar10", "cifar100"])
+    parser.add_argument("--datapath", default="./data/",
+                        type=str, help="Path to the CIFAR-10/100 dataset")
+    parser.add_argument('--num-labeled', type=int,
+                        default=4000, help='Total number of labeled samples')
+
+    #Hyperparameters
+    parser.add_argument("--lr", default=0.1, type=float,
+                        help="The initial learning rate")
+    parser.add_argument("--momentum", default=0.9, type=float,
+                        help="Optimizer momentum")
+    parser.add_argument("--wd", default=0.0005, type=float,
+                        help="Weight decay")
+    parser.add_argument("--expand-labels", action="store_true",
+                        help="expand labels to fit eval steps")
+
+    #Training Configuration
+    parser.add_argument('--train-batch', default=64, type=int,
+                        help='train batchsize')
+    parser.add_argument('--test-batch', default=64, type=int,
+                        help='train batchsize')
+    parser.add_argument('--total-iter', default=1024*1, type=int,
+                        help='total number of iterations to run')
+    parser.add_argument('--iter-per-epoch', default=1024, type=int,
+                        help="Number of iterations to run per epoch")
+    parser.add_argument('--num-workers', default=1, type=int,
+                        help="Number of workers to launch during training")
+
+    parser.add_argument("--dataout", type=str, default="./path/to/output/",
+                        help="Path to save log files")
+
+    # Model Architecture
+    parser.add_argument("--model-depth", type=int, default=16,
+                        help="model depth for wide resnet")
+    parser.add_argument("--model-width", type=int, default=8,
+                        help="model width for wide resnet")
+    parser.add_argument('--threshold', type=float, default=0.95,
+                        help='Confidence Threshold for pseudo labeling')
+    parser.add_argument('--lambda', type=float, default=0.8,
+                        help='Coefficient for Unsupervised Loss')
+
+    # Add more arguments if you need them
+    # Describe them in help
+    # You can (and should) change the default values of the arguments
+
+    args = parser.parse_args()
+
+    main(args)
